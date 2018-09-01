3d2e7c8f8f535379a974240fe37afb646f911bff
Lorena <lorena@artlolo.com>
2018-09-01 09:56:00 -0400

    Adding P5 libraries

---
+++ Homework/p5/ClassTestDrawing/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+  background("#58FAF4");
+
+}
+
+function draw() {
+  // put drawing code here  
+  rect(185,200,30,75);
+  ellipse(200,200,80,80);
+  ellipse(220,190,15,5);
+  ellipse(180,190,15,5);
+  triangle(200,195,195,215,205,215);
+  ellipse(200,225,10,10);
+
+}
